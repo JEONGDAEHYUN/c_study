9e86dc3e7f52a83f92823df3250add89daef5df4
JEONGDAEHYUN <daehyeonj41@gmail.com>
2025-06-12 08:34:32 +0900

    6.12study

---
+++ 3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(int argc, char* argv[]){
+	FILE* wfp;
+	FILE* rfp;
+	char str[200];
+
+	if(argc != 3) {
+		printf("매개변수가 3개가 아닙니다.\n");
+		return;
+	}
+
+	rfp = fopen(argv[1], "r");
+	wfp = fopen(argv[2], "w");
+
+	for(;;) {
+		fgets(str, 199, rfp);
+		if(feof(rfp)) {
+			break;
+		}
+		fputs(str, wfp);
+	}
+
+	fclose(rfp);
+	fclose(wfp);
+}
