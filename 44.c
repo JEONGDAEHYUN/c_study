15cc3f8774f83264a9a5a1e236f281877df49860
JEONGDAEHYUN <daehyeonj41@gmail.com>
2025-08-06 12:50:34 +0900

    8.6study

---
+++ 44.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+	FILE *wfp;
+	int i, j;
+
+	wfp = fopen("c:\\c_study\\gugu.txt", "w");
+
+	for(i=2; i<=9; i++)
+	{
+		fprintf(wfp, "#Á¦%d´Ü#", i);
+	}
+
+	fprintf(wfp, "\n\n");
+
+	for(i=1; i<=9; i++)
+	{
+		for(j=1; j<=9; j++)
+		{
+			fprintf(wfp, "%2dx%2d=%2d", j, i, j*i);
+		}
+		fprintf(wfp, "\n");
+	}
+	fclose(wfp);
+}
