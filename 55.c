2461f4f8ca00d28a64a3341f0a55bbf8e41cec4a
JEONGDAEHYUN <daehyeonj41@gmail.com>
2025-08-11 13:55:05 +0900

    8.11study

---
+++ 55.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void gugu(int *a)
+{
+	for(int i=1; i<=9; i++)
+	{
+		printf("%3dx%3d=%3d\n",*a, i, *a * i);
+	}
+}
+
+void main()
+{
+	int a;
+
+	printf("출력하고 싶은 단을 입력 : ");
+	scanf("%d", &a);
+
+	gugu(&a);
+}
