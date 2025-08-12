1e3aa14ae82881845a0fcc527f46b19146bf20fd
JEONGDAEHYUN <daehyeonj41@gmail.com>
2025-08-12 10:20:18 +0900

    56.c

---
+++ 56.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	FILE *wfp;
+	int i, j;
+
+	wfp = fopen("c:\\c_study\\gugu.txt", "w");
+
+	for(i=2; i<=9; i++)
+	{
+		for(j=2; j<=9; j++)
+		{
+			fprintf(wfp, "%2dx%2d=%2d", j, i, j*i);
+		}
+		fprintf(wfp, "\n");
+	}
+
+	fclose(wfp);
+}
