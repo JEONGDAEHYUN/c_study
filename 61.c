00b11214739c3623f6da3821b05b542557c3731c
JEONGDAEHYUN <daehyeonj41@gmail.com>
2025-08-13 13:57:37 +0900

    8.13study

---
+++ 61.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+	int gugu[9][9];
+	int i, j;
+
+	for(i=0; i<9; i++) 
+	{
+		for(j=0; j<8; j++)
+		{
+			printf("%dx%d=%2d", j+2, i+1, gugu[i][j]);
+		}
+		printf("\n");
+	}
+}
